a95fcfa000337d3bd5c148b4e04d3b8ace658ec0
Mynul Ahsan <106093520+Mynul-07@users.noreply.github.com>
2024-11-26 08:03:56 +0600

    Add files via upload

---
+++ control_flow/switch.js
@@ -0,0 +1,35 @@
+// const month = 3
+
+// switch (month) {
+//     case 1:
+//         console.log('January')
+//         break;
+//     case 2:
+//         console.log('February')
+//         break;
+//     case 3:
+//         console.log('March')
+//         break;
+//     case 4:
+//         console.log('April')
+//         break;
+
+//     default:
+//         console.log('Default match')
+//         break;
+// }
+
+const month = 'Feb'
+
+switch (month) {
+    case 'Jan':
+        console.log('Jan')
+        break;
+    case 'Feb':
+        console.log('Feb')
+        break;
+
+    default:
+        console.log('not match')
+        break;
+}
