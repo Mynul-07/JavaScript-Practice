a8342cf4a891fff6aeb2f1a987167cafb3542a41
Mynul Ahsan <106093520+Mynul-07@users.noreply.github.com>
2024-12-01 05:55:00 +0600

    Add files via upload

---
+++ iterations/while_loop.js
@@ -0,0 +1,22 @@
+
+let index = 0
+
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+let myArr = ['flash', 'superman', 'spiderman']
+
+let arr = 0
+while (arr < myArr.length) {
+    //console.log(`Value is ${myArr[arr]}`);
+    arr++
+}
+
+let score = 1
+
+do {
+    console.log(`Value is ${score}`);
+    score++
+} while (score <= 10);
